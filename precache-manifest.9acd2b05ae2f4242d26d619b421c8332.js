a8db22addad06749200492da5bb05673ec2a9199
Atteroot <yuora556@gmail.com>
2020-10-21 13:45:59 +0300

    Updates

---
+++ precache-manifest.9acd2b05ae2f4242d26d619b421c8332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16833e87b5ac48bf66af44c65d03e102",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8055c2235884d4050118",
+    "url": "./static/css/main.b38f2411.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8055c2235884d4050118",
+    "url": "./static/js/main.0a5fd688.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
